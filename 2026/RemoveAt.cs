abe2e548c55f3e38b6f70f4dd32ca612a4020841
meuz67 <meuz6767@gmail.com>
2026-01-09 13:45:39 +0300

    RemoveAt

---
+++ 2026/RemoveAt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+        static int[] RemoveAt(ref int[] arr, int at)
+        {
+            int[] newArr = new int[arr.Length - 1];
+            for (int i = 0; i < at; i++)
+                newArr[i] = arr[i];
+
+            for (int i = at + 1; i < arr.Length; i++)
+                newArr[i - 1] = arr[i];
+            arr = newArr;
+            return arr;   
+        }
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            arr = RemoveAt(ref arr, 2);
+            PrintArr(arr);
+        }
+    }
+}
