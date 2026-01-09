f4dfce27f0978c0c95e2480ac54fdc7184c1d8b9
meuz67 <meuz6767@gmail.com>
2026-01-09 19:58:14 +0300

    StringUpAndLow

---
+++ 2026/StringUpAndLow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string UpAndLow(string text)
+        {
+            char[] result = new char[text.Length];
+            for (int i = 0; i < result.Length; i++) 
+            { 
+                if(i%2 == 0)
+                {
+                    result[i] = Char.ToUpper(text[i]);
+                }
+                else
+                {
+                    result[i] = Char.ToLower(text[i]);
+                }
+            }
+            return new string(result);
+        }
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string result = UpAndLow(text);
+            Console.WriteLine(result);
+        }
+    }
+}
