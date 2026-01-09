fc969c723072bac7eca4ab7e7485eff289aaa187
meuz67 <meuz6767@gmail.com>
2026-01-09 13:31:33 +0300

    Arr resize

---
+++ 2026/Arr Resize.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class ConsoleApp1
+{
+    static void PrintArr(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] +  " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static int[] ArrSize(ref int[] arr, int size)
+    {
+        int[] temp = new int[size];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            temp[i] = arr[i];
+        }
+        arr = temp;
+        return arr;
+    }
+        static void Main()
+    {
+        int[] arr = {1, 4, 6};
+        PrintArr(arr);
+        arr = ArrSize(ref arr, 5);
+        PrintArr(arr);
+    }
+}
