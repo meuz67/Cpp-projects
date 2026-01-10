7d0859f6cfb256ddfff21d86391ade502e300ab7
meuz67 <meuz6767@gmail.com>
2026-01-10 17:11:54 +0300

    Sum Closest

---
+++ 2026/S7um closest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void HelloWorld(string msg) { 
+            Console.WriteLine(msg); 
+        }
+        static void Main(string[] args)
+        {
+            HelloWorld("Console.WriteLine");
+        }
+    }
+}
