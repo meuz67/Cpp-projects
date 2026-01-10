020e44cfdc7f7ab69cd42d5ce2de40698f959539
meuz67 <meuz6767@gmail.com>
2026-01-10 13:25:23 +0300

    Enum nextDay

---
+++ 2026/Enum nextDay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        enum DayOfWeek 
+         {
+            Monday = 1, 
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+         }
+        static DayOfWeek GetNextDay(DayOfWeek day)
+        {
+            if(day < DayOfWeek.Sunday)
+            {
+                return day + 1;
+            }
+            else
+            {
+                return day;
+            }
+        }
+
+        static void Main()
+        {
+            DayOfWeek day = DayOfWeek.Monday;
+            day = GetNextDay(day);
+            Console.WriteLine(day);
+        }
+    }
+}
