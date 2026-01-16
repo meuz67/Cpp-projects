f6b7048fe7a2ed9091d3238b3f1b7d9c8abd43b0
meuz67 <meuz6767@gmail.com>
2026-01-16 17:50:11 +0300

    Polirfimoz

---
+++ 2026/December/Polimorfimoz.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp1;
+
+class Car
+{ 
+    virtual public void Drive()
+    {
+        Console.WriteLine("Driving");
+    }
+}
+
+class Person
+{
+    public void Drive(Car car)
+    {
+        car.Drive();
+    }
+}
+
+class SportCar : Car
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Driving fast");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Person person = new Person();
+        person.Drive(new SportCar());
+    }
+}
