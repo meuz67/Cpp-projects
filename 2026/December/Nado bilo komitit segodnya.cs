bc6d80d851f4a9b2a47040b7b51beae133db0a9d
meuz67 <meuz6767@gmail.com>
2026-01-17 21:50:30 +0300

    Nuzhno bilo sdelyat komit
    Komit govno, ya znayu

---
+++ 2026/December/Nado bilo komitit segodnya.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Security;
+
+namespace ConsoleApp1
+{
+    class Counter
+    {
+        public int Value { get; set; }
+
+        public static Counter operator +(Counter c1, Counter c2)
+        {
+            return new Counter { Value = c1.Value + c2.Value };
+        }
+
+        public static bool operator >(Counter c1, Counter c2)
+        {
+            return c1.Value > c2.Value;
+        }
+
+        public static bool operator <(Counter c1, Counter c2)
+        {
+            return c1.Value < c2.Value;
+        }
+
+        public static int operator +(Counter c1, int c2)
+        {
+            return c1.Value + c2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter {Value = 55 };
+            Counter counter2 = new Counter {Value = 66 };
+            int result = counter1 + 55;
+            Console.WriteLine(result);
+        }
+    }
+}
