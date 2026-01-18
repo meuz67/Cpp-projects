309ab6b94aade66c046490cc006fa35a8751a0c2
meuz67 <meuz6767@gmail.com>
2026-01-18 17:26:26 +0300

    delegate
    POMOGITE NET SVETA PO 20 SYKA CHASOV, YA BLYAD KRITICHNYA INFRASTRUCTURA, YA ZHIVU NAPROTIV EBUCHEGO VOKZALA!!! VZRIVOV NE BILO UZHE MESYAC

---
+++ 2026/December/delegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp1;
+class Program
+{
+    static void DoOperation(int a, int b, Operation op)
+    {
+        Console.WriteLine(op(a,b));
+    }
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+    delegate int Operation(int a, int b);
+    static void Main(string[] args)
+    {
+        Operation op;
+        op = Subtract;
+        DoOperation(1,2,op);
+    }
+}
