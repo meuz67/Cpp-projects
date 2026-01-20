4ea695cfc7fba3c3d6bbb3d431f1b16595d1f3b9
meuz67 <meuz6767@gmail.com>
2026-01-20 21:22:32 +0300

    10 raz komichu tozhe samoe

---
+++ 2026/December/Nasral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConsoleApp1;
+class Program
+{
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    static int Minus(int a, int b)
+    {
+        return a - b;
+    }
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+    public delegate int Option(int a, int b);
+    static void Main(string[] argsStrings)
+    {
+        Option sum = Sum;
+        int result = sum(5, 10);
+        Console.WriteLine(result);
+    }
+}
