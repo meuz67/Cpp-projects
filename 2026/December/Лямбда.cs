bb886e181cdd1444f375e382c8ed172de4044ada
meuz67 <meuz6767@gmail.com>
2026-01-22 21:04:02 +0300

    Лямбда

---
+++ 2026/December/Лямбда.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var add = (int x, int y) =>
+            {
+                return x + y;
+            };
+            Console.WriteLine(add(1, 2));
+        }
+    }
+}
