ea57428f29d2acf829fd1026c7a7e7eddb222b06
meuz67 <meuz6767@gmail.com>
2026-01-22 20:32:02 +0300

    Четвертый коммит

---
+++ 2026/December/Четвертый коммит.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    class bank
+    {
+        public delegate void MyDelegate(string message);
+        private int sum {  get; set; }
+        public MyDelegate BankDelegate { get; set; }
+        public void MoreMoney(int moreMoney)
+        {
+            sum+= moreMoney;
+            BankDelegate?.Invoke($"You have {sum}");
+        }
+        public void LessMoney(int lessMoney)
+        {
+            sum-= lessMoney;
+            BankDelegate?.Invoke($"You have {sum}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bank account = new bank();
+            account.BankDelegate = WriteMessage;
+            account.MoreMoney(5);
+        }
+        public static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
