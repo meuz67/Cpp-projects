0e24bd50be9a0f018f9e174de1b0a95e2ebd7660
meuz67 <meuz6767@gmail.com>
2026-01-24 21:22:03 +0300

    Ping pong event

---
+++ 2026/December/PingPong event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    public delegate void NotifyHandler();
+    class Ping
+    {
+         public event NotifyHandler OnPing;
+         public void GetPing()
+         {
+             Console.WriteLine("Ping received Pong");
+             OnPing?.Invoke();
+         }
+    }
+    class Pong
+    {
+        public event NotifyHandler OnPong;
+        public void GetPong()
+        {
+            Console.WriteLine("Pong received Ping");
+            OnPong?.Invoke();
+        }
+    }
+    static void Main(string[] args)
+    {
+        Ping ping = new Ping();
+        Pong pong = new Pong();
+        ping.OnPing += pong.GetPong;
+        pong.OnPong += ping.GetPing;
+        ping.GetPing();
+    }
+}
