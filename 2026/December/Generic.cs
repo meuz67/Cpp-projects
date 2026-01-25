4ca89f55eda17ae62688ac1a177eda9d17b63f7d
meuz67 <meuz6767@gmail.com>
2026-01-25 12:14:53 +0300

    Generic

---
+++ 2026/December/Generic.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3 };
+            double[] doubleArr = { 1.0, 2.0, 3.0 };
+            string[] stringArr = { "1", "2", "3" };
+            displayElement(intArr);
+            displayElement(doubleArr);
+            displayElement(stringArr);
+            Console.ReadKey();
+        }
+        public static void displayElement<T>(T[] arr)
+        {
+            foreach (T i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
