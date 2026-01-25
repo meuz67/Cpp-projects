91b2676821feb9f9f6a1537fd7408fc58f141edf
meuz67 <meuz6767@gmail.com>
2026-01-25 15:25:35 +0300

    Try and catch

---
+++ 2026/December/Try and catch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace  program
+{
+    class Person
+    {
+        private int age;
+        public string Name { get; set; } = "";
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 18) 
+                    throw new Exception("Less than 18");
+                else
+                    age = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person Tom = new Person { Name = "Tom", Age = 17 };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
