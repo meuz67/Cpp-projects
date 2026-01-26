df33a8aa11cee918a2b7cb43707079f61bb581f0
meuz67 <meuz6767@gmail.com>
2026-01-26 23:21:27 +0300

    Caesar shift

---
+++ 2026/December/Caesar shift.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(Refrased(text, -3));
+        }
+
+        static string Refrased(string text, int change)
+        {
+            string updatedText = "";
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            try
+            {
+                foreach (var c in text)
+                {
+                    int temp = alphabet.IndexOf(c);
+                    updatedText += alphabet[temp + change];
+                }
+
+                return updatedText;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Mistake");
+            }
+        }
+    }
+}
