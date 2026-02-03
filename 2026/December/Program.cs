c346729f3408853b16304713ab9d105621495789
meuz67 <meuz6767@gmail.com>
2026-02-03 21:03:26 +0300

    Add files via upload

--- 2026/December/Program.cs
+++ 2026/December/Program.cs
@@ -1,48 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using System.Threading;
 namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
-        public delegate void PrintResult(string message);
-        static double Centre(int[] arr1, int[] arr2)
+        static int x = 0;
+        static object locker = new();
+        static void Count()
         {
-            double sum = 0;
-            int[] arr3 = new int[arr1.Length + arr2.Length];
-            for (int i = 0; i < arr1.Length; i++)
-                arr3[i] = arr1[i];
-
-            for (int i = 0; i < arr2.Length; i++)
-                arr3[i + arr1.Length] = arr2[i];
-            for (int i = 0; i < arr3.Length; i++)
+            x = 1;
+            lock (locker)
             {
-                sum += arr3[i];
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+                    x++;
+                    Thread.Sleep(1000);
+                }
             }
-            double result = sum / arr3.Length;
-            return result;
         }
         static void Main(string[] args)
         {
-            PrintResult printer = Print;
-            int[] arr1 = { 1, 2, 3, 4, };
-            int[] arr2 = { 3, 4, 5, 6, };
-            PrintArr(arr1);
-            PrintArr(arr2);
-            Print(Centre(arr1, arr2).ToString());
-        }
-        static void PrintArr(int[] arr)
-        {
-            foreach (int i in arr)
+            int x = 1;
+            for (int i = 0; i < 10; i++)
             {
-                Console.Write(i + " ");
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Thread" + i;
+                myThread.Start();
             }
-            Console.WriteLine();
-        }
-        static void Print(string Message) 
-        {  
-            Console.WriteLine(Message); 
         }
     }
 }
-
