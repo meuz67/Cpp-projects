3f249dd83331fbeb25eb8ac75e290a41e79b3ca5
meuz67 <meuz6767@gmail.com>
2026-02-24 15:01:47 +0000

    Selection Sort

---
+++ 2026/February/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+class Program
+{
+    static void SelectionSort(int[] unsorted)
+    {
+        int smallestIndex;
+        for (int i = 0; i < unsorted.Length - 1; i++)
+        {
+            smallestIndex = i;
+            for (int j = i + 1; j < unsorted.Length; j++)
+            {
+                if (unsorted[j] < unsorted[smallestIndex])
+                {
+                    smallestIndex = j;
+                }
+            }
+
+            if (smallestIndex != i)
+            {
+                int temp = unsorted[i];
+                unsorted[i] = unsorted[smallestIndex];
+                unsorted[smallestIndex] = temp;
+            }
+    }
+    }
+    static void Main()
+    {
+        int[] unsorted = { 1, 254, 4534, 3, 654, 459, 764 };
+        SelectionSort(unsorted);
+        Console.WriteLine(string.Join(", ", unsorted));
+    }
+}
