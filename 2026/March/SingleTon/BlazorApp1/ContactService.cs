f881f6a9fbe96b25f527f3a5a71ec6d8439c28d2
meuz67 <meuz6767@gmail.com>
2026-03-12 17:35:55 +0300

    SingleTon

---
+++ 2026/March/SingleTon/BlazorApp1/ContactService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1;
+
+public class ContactService
+{
+    public List<Contact> ContactList = new List<Contact>();
+
+    public void AddContact(Contact contact)
+    {
+        ContactList.Add(contact);
+    }
+}
