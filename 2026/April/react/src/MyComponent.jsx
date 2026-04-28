2de731bddae137535c841c738c6e4e660ae4cc62
meuz67 <meuz6767@gmail.com>
2026-04-28 16:32:52 +0300

    color

--- 2026/April/react/src/MyComponent.jsx
+++ 2026/April/react/src/MyComponent.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import "./MyComponent.css"
 function MyComponent() {
-    const[firstValue, setFirstValue] = useState(0);
-    const [secondValue, setSecondValue] = useState(0);
+    function handleColorChange(e){
+        setColor(e.target.value)
+    }
+    const [color, setColor] = useState("#FFFFF")
     return (
         <div>
-            <input type="number" onChange={(e) => setFirstValue(e.target.value)} value={firstValue} />
-            <input type="number" onChange={(e) => setSecondValue(e.target.value)} value={secondValue} />
-            <p>{firstValue * secondValue}</p>
+            <div className="cube" style={{ backgroundColor: color }}></div>
+            <div className="colorInput">
+                <input type="color" onChange={handleColorChange} value={color} />
+            </div>
         </div>
     );
 }
