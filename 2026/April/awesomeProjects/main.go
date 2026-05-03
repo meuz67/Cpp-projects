a21de0bdb5995558748a5d211bb91958cb3e326a
meuz67 <meuz6767@gmail.com>
2026-05-03 12:19:42 +0300

    bank

--- 2026/April/awesomeProjects/main.go
+++ 2026/April/awesomeProjects/main.go
@@ -5,53 +5,58 @@ import (
 	"io"
 	"net/http"
 	"strconv"
-	"sync"
+	"sync/atomic"
 )
 
-var mtx = sync.Mutex{}
-var money = 1000
-var bankMoney = 0
+var money = atomic.Int64{}
+var bankMoney = atomic.Int64{}
 
 func payHandler(w http.ResponseWriter, r *http.Request) {
-	httpRequstBody, err := io.ReadAll(r.Body)
+	httpRequestBodyString, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Error reading body:", err)
+		fmt.Println("Failed to read request body")
+		http.Error(w, err.Error(), 400)
+		return
 	}
-	amount, err := strconv.ParseInt(string(httpRequstBody), 10, 64)
+	amount, err := strconv.Atoi(string(httpRequestBodyString))
 	if err != nil {
 		fmt.Println("Failed to parse amount")
+		http.Error(w, err.Error(), 400)
+		return
 	}
-	mtx.Lock()
-	if amount <= int64(money) {
-		money -= int(amount)
+	if int64(amount) <= money.Load() {
+		money.Add(int64(-amount))
 	}
-	mtx.Unlock()
-	fmt.Println("Money:", money)
+	fmt.Println("amount:", money.Load())
+	w.WriteHeader(http.StatusOK)
 }
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	httpRequestBody, err := io.ReadAll(r.Body)
+	httpRequestBodyString, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Error reading body:", err)
+		fmt.Println("Failed to read request body")
+		http.Error(w, err.Error(), 400)
+		return
 	}
-	amount, err := strconv.ParseInt(string(httpRequestBody), 10, 64)
+	amount, err := strconv.Atoi(string(httpRequestBodyString))
 	if err != nil {
 		fmt.Println("Failed to parse amount")
+		http.Error(w, err.Error(), 400)
+		return
 	}
-	mtx.Lock()
-	if amount <= int64(money) {
-		money -= int(amount)
-		bankMoney += int(amount)
+	if int64(amount) <= money.Load() {
+		bankMoney.Add(int64(amount))
+		money.Add(int64(-amount))
 	}
-	mtx.Unlock()
-	fmt.Println("Money:", money)
-	fmt.Println("Bank money:", bankMoney)
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("bank money:", bankMoney.Load())
+	fmt.Println("money:", money.Load())
 }
 func main() {
-	http.HandleFunc("/", payHandler)
+	money.Add(1000)
+	http.HandleFunc("/pay", payHandler)
 	http.HandleFunc("/save", saveHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Failed to start server")
-	}
 
+	}
 }
