f6ce3d48c3cd671b70cd1e011e92ee7d5c141652
meuz67 <meuz6767@gmail.com>
2026-05-09 22:23:33 +0300

    Create and read

--- 2026/May/awesomeProjects/main.go
+++ 2026/May/awesomeProjects/main.go
@@ -1,45 +1,76 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+
+	_ "github.com/lib/pq"
 )
 
-type User struct {
-	FirstName string
-	LastName  string
-	Age       int
-}
-type Users struct {
-	Users []User
+type Product struct {
+	Id      int
+	Model   string
+	Company string
+	Price   int
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	u1 := User{
-		FirstName: "John",
-		LastName:  "Doe",
-		Age:       25,
+var db *sql.DB
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmp, err := template.ParseFiles("create.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmp.Execute(w, nil)
+		return
 	}
-	u2 := User{
-		FirstName: "Jane",
-		LastName:  "Doe",
-		Age:       45,
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		model := r.PostFormValue("model")
+		company := r.PostFormValue("company")
+		price, err := strconv.Atoi(r.PostFormValue("price"))
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+		}
+		db.Exec(`INSERT INTO products (model, company, price) VALUES ($1, $2, $3)`, model, company, price)
+		http.Redirect(w, r, "/", http.StatusFound)
 	}
-	u3 := User{
-		FirstName: "Jane",
-		LastName:  "Doe",
-		Age:       67,
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM products")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
 	}
-	data := Users{
-		Users: []User{u1, u2, u3},
+	defer rows.Close()
+	users := make([]Product, 0)
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.Id, &p.Model, &p.Company, &p.Price)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+		users = append(users, p)
 	}
-	tmp := template.Must(template.ParseFiles("index.html"))
-	tmp.Execute(w, data)
+	tmp1 := template.Must(template.ParseFiles("index.html"))
+	tmp1.Execute(w, users)
 }
 func main() {
-	http.HandleFunc("/", handler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	var err error
+	db, err = sql.Open("postgres", "user=postgres password=sar58yeaf dbname=postgres sslmode=disable")
+	if err != nil {
 		fmt.Println(err.Error())
 	}
+	defer db.Close()
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/create", createHandler)
+	http.ListenAndServe(":8080", nil)
 }
