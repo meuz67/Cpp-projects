ea99759ac9c547125f6f2d1fb076ffe2da9bedaf
meuz67 <meuz6767@gmail.com>
2026-05-10 16:44:20 +0300

    crud

--- 2026/May/awesomeProjects/main.go
+++ 2026/May/awesomeProjects/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -19,58 +20,116 @@ type Product struct {
 
 var db *sql.DB
 
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	db.Exec("DELETE FROM products WHERE id = $1", id)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+func editPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	product := Product{}
+	err = db.QueryRow("SELECT * FROM products WHERE id=$1", id).Scan(&product.Id,
+		&product.Model, &product.Company, &product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tmp, err := template.ParseFiles("edit.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tmp.Execute(w, product)
+}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	id := vars["id"]
+	model := r.FormValue("model")
+	company := r.FormValue("company")
+	price := r.FormValue("price")
+	db.Exec("UPDATE products SET model=$1, company=$2, price=$3 WHERE id=$4", model, company, price, id)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
 func createHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		tmp, err := template.ParseFiles("create.html")
 		if err != nil {
-			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		tmp.Execute(w, nil)
-		return
 	}
 	if r.Method == "POST" {
 		err := r.ParseForm()
 		if err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		model := r.PostFormValue("model")
 		company := r.PostFormValue("company")
-		price, err := strconv.Atoi(r.PostFormValue("price"))
-		if err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-		}
-		db.Exec(`INSERT INTO products (model, company, price) VALUES ($1, $2, $3)`, model, company, price)
+		price := r.PostFormValue("price")
+		db.Exec("INSERT INTO products (model, company, price) VALUES ($1, $2, $3)", model, company, price)
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * FROM products")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	sort := r.URL.Query().Get("sort")
+	query := "SELECT * FROM products"
+	switch sort {
+	case "price_asc":
+		query += " ORDER BY price ASC"
+		break
+	case "price_desc":
+		query += " ORDER BY price DESC"
+		break
 	}
-	defer rows.Close()
-	users := make([]Product, 0)
-	for rows.Next() {
-		var p Product
-		err = rows.Scan(&p.Id, &p.Model, &p.Company, &p.Price)
+	result, err := db.Query(query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	var products []Product
+	for result.Next() {
+		p := Product{}
+		err := result.Scan(&p.Id, &p.Model, &p.Company, &p.Price)
 		if err != nil {
-			fmt.Fprintf(w, err.Error())
-			return
+			fmt.Println(err.Error())
 		}
-		users = append(users, p)
+		products = append(products, p)
 	}
 	tmp1 := template.Must(template.ParseFiles("index.html"))
-	tmp1.Execute(w, users)
+	tmp1.Execute(w, products)
 }
 func main() {
+	connStr := "user=postgres password=sar58yeaf dbname=postgres sslmode=disable"
 	var err error
-	db, err = sql.Open("postgres", "user=postgres password=sar58yeaf dbname=postgres sslmode=disable")
+	rout := mux.NewRouter()
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer db.Close()
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/create", createHandler)
+	rout.HandleFunc("/", mainHandler)
+	rout.HandleFunc("/create", createHandler)
+	rout.HandleFunc("/edit/{id:[0-9]+}", editPage).Methods("GET")
+	rout.HandleFunc("/edit/{id:[0-9]+}", editHandler).Methods("POST")
+	rout.HandleFunc("/delete/{id:[0-9]+}", deleteHandler)
+	http.Handle("/", rout)
 	http.ListenAndServe(":8080", nil)
 }
