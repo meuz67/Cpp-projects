8434221dce0b586e82b50205beb288b8c3ae1593
meuz67 <meuz6767@gmail.com>
2026-05-29 15:54:24 +0300

    python

--- 2026/May/awesomeProject1/main.go
+++ 2026/May/awesomeProject1/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	foo(logger)
+}
+func foo(log *zap.Logger) {
+	log.Error("foo")
+}
