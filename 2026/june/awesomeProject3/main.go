1f1704256a6bbfc31d3d65e0a6079893820a17ea
meuz67 <meuz6767@gmail.com>
2026-06-17 13:05:51 +0300

    move zeros

--- 2026/june/awesomeProject3/main.go
+++ 2026/june/awesomeProject3/main.go
@@ -1,25 +1,15 @@
 package main
 
-func mergeArr(nums1 []int, nums2 []int) []int {
-	p1 := 0
-	p2 := 0
-	nums3 := []int{}
-	for p1 < len(nums1) && p2 < len(nums2) {
-		if nums1[p1] < nums2[p2] {
-			nums3 = append(nums3, nums1[p1])
-			p1++
+func moveZeros(nums []int) []int {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
 		}
-		if nums1[p1] > nums2[p2] {
-			nums3 = append(nums3, nums2[p2])
-			p2++
-		}
-		if nums1[p1] == nums2[p2] {
-			nums3 = append(nums3, nums1[p1])
-			p1++
-			p2++
-		}
+		fast++
 	}
-	return nums3
+	return nums
 }
 func main() {
 }
